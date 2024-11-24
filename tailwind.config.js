cd34200d7bbb9718d14dab6a10969b2a641301ab
Danial Alborz <danialalborz3@gmail.com>
2024-11-24 19:20:44 +0330

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': 'montserrat',
+        'poppins': 'poppins',
+        'bebasNeue': 'bebasNeue',
+      },
+      // keyframes:{
+      //   rotate : {
+      //     '0%': {
+      //       transform: 'rotate(0deg)',
+      //     },
+      //     '50%': {
+      //       transform: 'rotate(90deg)',
+      //     },
+      //     '100%': {
+      //       transform: 'rotate(0deg)',
+      //     }
+      //   }
+      // },
+      // animation:{
+      //   'rotate-animation': 'rotate-animation 5s ease-in-out',
+      // }
+    },
+  },
+  plugins: [],
+}
+
